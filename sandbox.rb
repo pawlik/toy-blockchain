c20837295db4d5863831b17450873345b7c6ca1a
Grzegorz Pawlik <grzegorzpawlik@gmail.com>
2018-03-25 14:46:29 +0200

    Add some notes regarding block structure

--- sandbox.rb
+++ sandbox.rb
@@ -5,34 +5,34 @@ require 'json'
 require 'pry'
 require 'pry-doc'
 
-# block structure would be: 
+# block structure would be:
 
-# 
-
-{
-    transactions: {
-        '0' => {from: '', to: 'miner_key.pub', amount: 100}, # miner's reward
-        '1' => {from: 'sender_key.pub', to: 'receiver_key.pub', amount: 123, payload: '', signature: ''},
-        # transactions have indexes from 1 to 10, block with more are invalid
+block = {
+  transactions: {
+    '0' => { from: '', to: 'miner_key.pub', amount: 100 }, # miner's reward
+    '1' => {
+      from: 'sender_key.pub',
+      to: 'receiver_key.pub',
+      amount: 123,
+      payload: '',
+      signature: ''
     },
-    transactions_hash: 'merkle_hash_of_transactions',
-    timestamp: Time.now.utc.to_i, 
-    allowed_miners: [ # only in gensis block?
-        {key: 'public_key', address: '0.0.0.0:12345'},
-        {key: 'other_public_key', address: '0.0.0.0:12346'},
-    ]
+    # transactions have indexes from 1 to 10, block with more are invalid
+    # to verify transaction, replace signature with empty string, and verify it's JSON representation?
+  },
+  transactions_hash: 'merkle_hash_of_transactions',
+  timestamp: Time.now.utc.to_i,
+  allowed_miners: [ # only in gensis block?
+    { key: 'public_key', address: '0.0.0.0:12345' },
+    { key: 'other_public_key', address: '0.0.0.0:12346' }
+  ],
+  signature: 'XXX' # to verify block, replace signature with empty string, and verify signature
 }
-
-
-Block = OpenStruct.new(:
-
-data = {z: 'z', g: 'g'}
 
 string = data.to_json
 
 foo = OpenSSL::PKey::RSA.new(File.read('id_rsa_1'), '')
 binding.pry
-
 
 signed = foo.sign_pss('SHA256', string, salt_length: :max, mgf1_hash: 'SHA256')
 
