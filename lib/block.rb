b4afc78c9dfb4985735f3fe2504047dbe91cd2ea
Grzegorz Pawlik <grzegorzpawlik@gmail.com>
2018-03-28 13:43:58 +0200

    Make block's hash elements required

--- lib/block.rb
+++ lib/block.rb
@@ -14,24 +14,24 @@ class Block
   end
 
   def initialize(hash = {})
-    @signature = hash[:signature]
-    @transactions = hash[:transactions]
-    @transactions_hash = hash[:transactions_hash]
-    @timestamp = hash[:timestamp]
-    @allowed_miners = hash[:allowed_miners]
-    @height = hash[:height]
-    @previous_hash = hash[:previous_hash]
-    @signed_by = hash[:signed_by]
-    @signature = hash[:signature]
+    @signature = hash.fetch(:signature)
+    @transactions = hash.fetch(:transactions)
+    @transactions_hash = hash.fetch(:transactions_hash)
+    @timestamp = hash.fetch(:timestamp)
+    @allowed_miners = hash.fetch(:allowed_miners)
+    @height = hash.fetch(:height)
+    @previous_hash = hash.fetch(:previous_hash)
+    @signed_by = hash.fetch(:signed_by)
+    @signature = hash.fetch(:signature)
   end
 
   def self.from_hash(hash)
     new(hash)
   end
 
-  # I have a feeling that real implementation
+  # The real implementation
   # would need canonical block as string representation
-  # but I'll keep it just as ruby's JSON to keep the project simple.
+  # right now we assume that to_json does not change the order of stuff
   def to_json
     {
       transactions: @transactions,
